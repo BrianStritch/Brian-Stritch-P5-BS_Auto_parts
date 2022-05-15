5600ed07a80f1413af91f80308334c2ea21761f0
BrianStritch <brians12ca18@gmail.com>
2022-05-15 00:26:24 +0000

    added sorting javascript and back to top button

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models.functions import Lower
 from django.db.models import Q # this is django built in and generates a search query
 from .models import Product, Category, Manufacturer
 
@@ -25,7 +26,7 @@ def all_products(request):
         sort = sortkey
         if sortkey == 'name':
           sortkey = 'lower_name'
-          products = products.annotate(lower_name = lower('name'))
+          products = products.annotate(lower_name = Lower('name'))
         if sortkey == 'category':
                 sortkey = 'category__name'
           
