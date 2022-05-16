eb42eff64321112da66231f87466ae3f98c25b21
BrianStritch <brians12ca18@gmail.com>
2022-05-16 21:12:33 +0000

    fix bug in search nav icon logic in bag view

--- bag/views.py
+++ bag/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from products.models import Product, Category, Manufacturer
 
-# Create your views here.
 
-def view_bag(request):
+def view_bag(request):    
     """ A view to return the shopping bag contents page """
 
-    return render(request, 'bag/bag.html')
+    products = Product.objects.all()
+    makes = Manufacturer.objects.all().order_by('name')
+
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+        
+        
+        current_sorting = f'{sort}_{direction}'
+
+        context = {
+        'products': products,
+        'makes':makes,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+        }
+        return render(request, 'products/products.html', context)
+
+    else:
+
+        return render(request, 'bag/bag.html')
