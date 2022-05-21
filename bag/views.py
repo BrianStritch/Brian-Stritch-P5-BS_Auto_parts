f20ba581aa45b0db70e0493deef6f5db58aeff3c
BrianStritch <brians12ca18@gmail.com>
2022-05-21 23:45:11 +0000

    add functionality to add items to the shopping bag, ammend styling issues across all pages

--- bag/views.py
+++ bag/views.py
@@ -42,6 +42,7 @@ def view_bag(request):
 
         return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ add a quantity of the selected product to the shopping bag """
 
@@ -69,3 +70,29 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag 
     return redirect(redirect_url)
+
+
+def edit_bag(request, item_id):
+    """ edit a quantity of the selected product in the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop[item_id]
+
+    request.session['bag'] = bag 
+    return redirect(reverse('view_bag'))
