a86f9ec60876495b08e0076e089e05695571d48b
BrianStritch <brians12ca18@gmail.com>
2022-05-22 22:07:26 +0000

    added toasts and messages for relevant actions

--- bag/views.py
+++ bag/views.py
@@ -46,6 +46,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ add a quantity of the selected product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -58,15 +59,20 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'You have succesfully added another "{product.name}" size "{size}" to the shopping bag.')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'You have succesfully added "{product.name}" size "{size}" to the shopping bag.')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'You have succesfully added "{quantity}" "{product.name}" size "{size}" to the shopping bag.')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'You have succesfully added another "{product.name}" to the shopping bag.')
         else:
             bag[item_id] = quantity
+            messages.success(request, f'You have succesfully added "{product.name}" to the shopping bag.')
 
     request.session['bag'] = bag 
     return redirect(redirect_url)
@@ -75,6 +81,7 @@ def add_to_bag(request, item_id):
 def edit_bag(request, item_id):
     """ edit a quantity of the selected product in the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
 
@@ -86,15 +93,20 @@ def edit_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'You have succesfully edited the quantity of "{product.name}" size "{size}" in your shopping bag.')
         else:
             del bag[item_id]['items_by_size'][size]
+            messages.success(request, f'You have succesfully removed "{product.name}" size "{size}" from your shopping bag.')
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                messages.success(request, f'You have succesfully removed "{product.name}" size "{size}" from your shopping bag.')
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'You have succesfully edited the quantity of "{product.name}" in your shopping bag.')
         else:
             bag.pop(item_id)
+            messages.success(request, f'You have succesfully removed "{product.name}" from your shopping bag.')
 
     request.session['bag'] = bag 
     return redirect(reverse('view_bag'))
@@ -104,6 +116,7 @@ def delete_bag_item(request, item_id):
     """ remove a selected product from the shopping bag """
 
     try:
+        product = Product.objects.get(pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -112,10 +125,13 @@ def delete_bag_item(request, item_id):
 
         if size:
             del bag[item_id]['items_by_size'][size]
+            messages.success(request, f'You have succesfully removed "{product.name}" size "{size}" from your shopping bag.')
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                messages.success(request, f'You have succesfully removed "{product.name}" size "{size}" from your shopping bag.')
         else:        
             bag.pop(item_id)
+            messages.success(request, f'You have succesfully removed "{product.name}" from your shopping bag.')
 
         request.session['bag'] = bag 
         return HttpResponse(status=200)
