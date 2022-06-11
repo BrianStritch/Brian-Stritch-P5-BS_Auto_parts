bed19e7bb6df35cc1381aa73df1dabd6bea7e435
BrianStritch <brians12ca18@gmail.com>
2022-06-11 16:26:58 +0000

    add delete product view and url and test functionality

--- products/views.py
+++ products/views.py
@@ -99,9 +99,9 @@ def add_product(request):
   if request.method == 'POST':
     form = ProductForm(request.POST, request.FILES)
     if form.is_valid():
-      form.save()
+      product = form.save()
       messages.success(request, 'Your product has been succesfully added.')
-      return redirect(reverse('add_product'))
+      return redirect(reverse('product_detail', args=[product.id ]))      
     else:
       messages.error(request, 'Failed to add product. Please check your form details.')
   else:
@@ -116,7 +116,7 @@ def add_product(request):
 
 def edit_product(request, product_id):
   """ 
-  Edit a product to the store
+  Edit a product in the store
   """
   product = get_object_or_404(Product, pk=product_id)  
 
@@ -140,3 +140,13 @@ def edit_product(request, product_id):
     'stop_toast_cart': True,
   }
   return render(request, template, context)
+  
+def delete_product(request, product_id):
+  """
+  Delete a product from the store
+  """
+  product = get_object_or_404(Product, pk=product_id)
+  product.delete()
+  messages.success(request, 'Your selected product has been removed from the store')
+  return redirect(reverse('products'))
+
