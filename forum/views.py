3427db57554d6712b4b86f830002c6fdf595789d
BrianStritch <brians12ca18@gmail.com>
2022-06-23 17:03:21 +0000

    added search functionality to forum page

--- forum/views.py
+++ forum/views.py
@@ -1,16 +1,60 @@
-from django.shortcuts import render
 from .models import ForumCategory, ForumPost, ForumPostComment, ForumTopics
 from forum.forms import ForumCategoryForm, ForumPostCommentForm, ForumTopicsForm, CreateForumPostForm
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from products.models import Product, Category, Manufacturer
 
 
 def forum(request):
+    products = Product.objects.all()
+    makes = Manufacturer.objects.all().order_by('name')
     template_name = 'forum/forum.html'
-    categories = ForumCategory.objects.all()
-    topics = ForumTopics.objects.all()
 
-    context = {
-        'home':True,
-        'categories': categories,
-        'topics': topics,
-    }
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+        
+        if 'make' in request.GET:
+            query = request.GET['make']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+        
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+        current_sorting = f'{sort}_{direction}'
+
+        context = {
+        'products': products,
+        'makes':makes,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+        }
+        return render(request, 'products/products.html', context)
+
+    else:
+        
+        categories = ForumCategory.objects.all()
+        topics = ForumTopics.objects.all()
+
+        context = {
+            'home':True,
+            'categories': categories,
+            'topics': topics,
+        }
     return render(request, template_name, context)
