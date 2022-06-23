d0ecfd00ddb3f0a612af1991acb1eef79549c156
BrianStritch <brians12ca18@gmail.com>
2022-06-23 00:13:53 +0000

    add content to forum page

--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from .models import ForumCategory, ForumPost, ForumPostComment, ForumTopics
+from forum.forms import ForumCategoryForm, ForumPostCommentForm, ForumTopicsForm, CreateForumPostForm
 def forum(request):
     template_name = 'forum/forum.html'
+    categories = ForumCategory.objects.all()
+    topics = ForumTopics.objects.all()
+
     context = {
-        'home':True
+        'home':True,
+        'categories': categories,
+        'topics': topics,
     }
     return render(request, template_name, context)
