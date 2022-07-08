bb916f9ddcf1cc31dec99d8c33ebc5765e86da1a
BrianStritch <brians12ca18@gmail.com>
2022-07-08 17:04:21 +0000

    fix search input fields on shop page

--- home/views.py
+++ home/views.py
@@ -12,25 +12,129 @@ def home(request):
     """
     A view to return the home page
     """
+    
     newsletter_form = NewsletterSignupForm()
-    template = 'home/index.html'
-    home = True
-    context = {
-        'form':newsletter_form,
-        'home': home,
-        'bttoff':True
-    }
-    return render(request, template, context)
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        products = Product.objects.all()
+        makes = Manufacturer.objects.all().order_by('name')
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+        if 'make' in request.GET:
+            query = request.GET['make']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+        current_sorting = f'{sort}_{direction}'
+
+        context = {
+        'newsletter_form':newsletter_form,
+        'products': products,
+        'makes':makes,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+        }
+        return render(
+            request, 'products/products.html', context)
+
+    else:
+        """
+        A view to return the home page
+        """
+
+        template = 'home/index.html'
+        home = True
+        context = {
+            'form':newsletter_form,
+            'home': home,
+            'bttoff':True
+        }
+        return render(request, template, context)
+
+    # template = 'home/index.html'
+    # home = True
+    # context = {
+    #     'form':newsletter_form,
+    #     'home': home,
+    #     'bttoff':True
+    # }
+    # return render(request, template, context)
 
 def shop(request):
     """
     A view to return the home page
     """
-    makes = Manufacturer.objects.all().order_by('name')   
+    makes = Manufacturer.objects.all().order_by('name')
+    products = Product.objects.all()
+    makes = Manufacturer.objects.all().order_by('name')
 
-    context = {
-        'makes': makes,
-        'bttoff':True,
-        
-    }
-    return render(request,'home/shop.html', context)    
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+        if 'make' in request.GET:
+            query = request.GET['make']
+            if not query:
+                messages.error(
+                request, "You didn't enter any search criteria!")
+                return redirect(reverse('products')) 
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+            current_sorting = f'{sort}_{direction}'
+
+            context = {
+            'products': products,
+            'makes':makes,
+            'search_term': query,
+            'current_categories': categories,
+            'current_sorting': current_sorting,
+            }
+            return render(request, 'products/products.html', context)
+
+    else:
+        """
+        A view to return the home page
+        """
+        makes = Manufacturer.objects.all().order_by('name')    
+
+        context = {
+            'makes': makes,
+            'bttoff':True,
+
+        }
+        return render(request,'home/shop.html', context)     
